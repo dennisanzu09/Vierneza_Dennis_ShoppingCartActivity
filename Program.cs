8f4d0c1a5ef83790844d158f5ffb2680c727434a
dennisanzu09 <dennisvierneza7@gmail.com>
2026-04-30 18:48:21 +0800

    Update Program.cs
    Add cart management features (view, update, remove, clear)

--- Program.cs
+++ Program.cs
@@ -28,26 +28,52 @@ class Program
         int[] qty = new int[10];
         int cartCount = 0;
 
-        Console.Write("Enter product number: ");
-        int choice = int.Parse(Console.ReadLine());
-
-        Console.Write("Enter quantity: ");
-        int quantity = int.Parse(Console.ReadLine());
-
-        Product selected = products[choice - 1];
-
-        if (selected.Stock >= quantity)
+        while (true)
         {
-            cart[cartCount] = selected;
-            qty[cartCount] = quantity;
-            cartCount++;
-            selected.Stock -= quantity;
+            Console.WriteLine("1 Add  2 View  3 Remove  4 Clear  5 Exit");
+            int op = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Added to cart!");
-        }
-        else
-        {
-            Console.WriteLine("Not enough stock.");
+            if (op == 1)
+            {
+                Console.Write("Product #: ");
+                int c = int.Parse(Console.ReadLine());
+
+                Console.Write("Qty: ");
+                int q = int.Parse(Console.ReadLine());
+
+                if (products[c - 1].Stock >= q)
+                {
+                    cart[cartCount] = products[c - 1];
+                    qty[cartCount++] = q;
+                    products[c - 1].Stock -= q;
+                }
+            }
+            else if (op == 2)
+            {
+                for (int i = 0; i < cartCount; i++)
+                    Console.WriteLine($"{i + 1}. {cart[i].Name} x{qty[i]}");
+            }
+            else if (op == 3)
+            {
+                Console.Write("Remove #: ");
+                int i = int.Parse(Console.ReadLine()) - 1;
+                cart[i].Stock += qty[i];
+
+                for (; i < cartCount - 1; i++)
+                {
+                    cart[i] = cart[i + 1];
+                    qty[i] = qty[i + 1];
+                }
+                cartCount--;
+            }
+            else if (op == 4)
+            {
+                for (int i = 0; i < cartCount; i++)
+                    cart[i].Stock += qty[i];
+
+                cartCount = 0;
+            }
+            else break;
         }
     }
 }
